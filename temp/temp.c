a6c073049453257f3ed392eb364757a68da7e8f3
dirtyryguy <91397609+dirtyryguy@users.noreply.github.com>
2022-07-21 13:12:40 -0400

    Add files via upload

--- temp/temp.c
+++ temp/temp.c
@@ -1,1 +1,19 @@
+#include "pico/stdlib.h"
+#include <stdio.h>
 
+int main(void)
+{
+	stdio_init_all();
+
+	while (!stdio_usb_connected())
+		sleep_ms(50);
+
+	int buf[2];
+	while (1)
+	{
+		scanf("%i,%i", buf, buf+1);
+		printf("%i,%i\n", *buf, *(buf+1));
+	}
+
+	return 0;
+}
